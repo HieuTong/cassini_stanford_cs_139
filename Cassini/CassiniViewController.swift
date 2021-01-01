e81d327f5f3bc6e227f43eaa1f9034c7cbeade57
HieuTong <hieu.le@coinhako.com>
2021-01-02 00:48:33 +0700

    thread

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Le Tong Minh Hieu on 1/1/21.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let url = DemoURLs.NASA[identifier] {
+                if let imageVC = segue.destination.contents as? ImageViewController {
+                    imageVC.imageURL = url
+                    imageVC.title = (sender as? UIButton)?.currentTitle
+                }
+            }
+        }
+    }
+    
+
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
